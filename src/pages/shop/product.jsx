6964207cc05b27b8db13bab9900142bf49cdf171
sabrina <khazem.sabrina@email.com>
2025-04-15 15:40:20 +0100

    my

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Product = (props) => {
+  const { id, productsName, price, productImage } = props.data;
+  return <div>product</div>;
+};
+
+export default Product;
---
+++ src/products.js
@@ -0,0 +1,25 @@
+import product1 from "./assets/1.png";
+import product2 from "./assets/2.png";
+
+import product4 from "./assets/4.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productsName: "Iphone 1",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productsName: "Iphone 2",
+    price: 999.0,
+    productImage: product2,
+  },
+  {
+    id: 4,
+    productsName: "Iphone 4",
+    price: 999.0,
+    productImage: product4,
+  },
+];
